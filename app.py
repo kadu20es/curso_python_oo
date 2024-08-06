0b4eb92d0e8c1307264ec131e87cfc9737a0adb2
030093457 <ceval@banestes.com.br>
2024-08-06 09:46:38 -0300

    Composicao - classe restaurante e avaliacao

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from modelos import restaurante
 
 os.system('clear')
 
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,4 @@
-from avaliacao import Avaliacao
+from modelos.avaliacao import Avaliacao
 
 # cria uma classe
 class Restaurante:
@@ -14,17 +14,17 @@ class Restaurante:
 
     # método especial do python - imprime os dados do retorno apenas chamando o nome da variável de instância
     def __str__(self):
-        return f'Restaurante: {self.nome} | Categoria: {self.categoria} | Status: {'ativo' if self._ativo else 'inativo'}'
+        return f'Restaurante: {self.nome} | Categoria: {self.categoria} | Status: {'ativo' if self._ativo else 'inativo'} | Nota: {self.media_avaliacao}'
     
     # método de classe
     @classmethod
     def listar_restaurantes(cls):
-        print('+---------------------------+---------------------------+------------------------+')
-        print(f'| {'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status (ativo/inativo) |'}')
-        print('----------------------------+---------------------------+------------------------+')
+        print('+---------------------------+---------------------------+---------------------------+------------------------+')
+        print(f'| {'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status (ativo/inativo)'.ljust(25)} | {'Avaliação'.ljust(22)} |')
+        print('----------------------------+---------------------------+---------------------------+------------------------+')
         for restaurante in Restaurante.restaurantes:
-            print(f'| {restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo.ljust(22)} |')
-        print('+---------------------------+---------------------------+------------------------+')
+            print(f'| {restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo.ljust(25)} | {str(restaurante.media_avaliacao).ljust(22)} |')
+        print('+---------------------------+---------------------------+---------------------------+------------------------+')
     
     @property
     def ativo(self):
@@ -36,3 +36,13 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return 0
+        
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
